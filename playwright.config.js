0f18c415882984cbc8c21b1c1182519ca8f53443
Dominiiic <dominicocarolino15@gmail.com>
2026-02-02 20:52:21 +0800

    update reporter to json

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,9 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['json', { outputFile: 'playwright-report/index.json' }],
+  ], 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -34,7 +36,6 @@ export default defineConfig({
     headless: false,
   },
 
-  /* Configure projects for major browsers */
   projects: [
     {
       name: 'Setup Authentication',
